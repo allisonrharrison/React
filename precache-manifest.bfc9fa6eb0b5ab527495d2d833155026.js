71253b2ddc9015865beac9810c92d35a8453ba1a
Allison Harrison <allisonrharrison@gmail.com>
2020-02-06 00:55:05 -0800

    Updates

---
+++ precache-manifest.bfc9fa6eb0b5ab527495d2d833155026.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999deea1437169cc9c54e23cbf1896a2",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "9d736a1ec5ad0450c170",
+    "url": "/React/static/css/2.28401111.chunk.css"
+  },
+  {
+    "revision": "c58e2d4e5d46f00f1873",
+    "url": "/React/static/css/main.2b5dc33c.chunk.css"
+  },
+  {
+    "revision": "9d736a1ec5ad0450c170",
+    "url": "/React/static/js/2.ac5ff2c6.chunk.js"
+  },
+  {
+    "revision": "17b328152e081a2c016e5e33d790b6fe",
+    "url": "/React/static/js/2.ac5ff2c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58e2d4e5d46f00f1873",
+    "url": "/React/static/js/main.9618d860.chunk.js"
+  },
+  {
+    "revision": "a74735d5108700b2fff1",
+    "url": "/React/static/js/runtime-main.dc7f6b9b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
