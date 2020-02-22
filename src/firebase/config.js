580c265f28ab1abfda6e40923754431116dd4227
Allison Harrison <allisonrharrison@gmail.com>
2020-02-21 18:49:49 -0800

    Integrate Client with Firebase Server

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyADcReyq7sjXTeqotojTvCCD0NjbOry8h0",
+  authDomain: "confusionserver-913e7.firebaseapp.com",
+  databaseURL: "https://confusionserver-913e7.firebaseio.com",
+  projectId: "confusionserver-913e7",
+  storageBucket: "confusionserver-913e7.appspot.com",
+  messagingSenderId: "952059553444",
+  appId: "1:952059553444:web:ad7c155ba1bbc1c2a9a831",
+  measurementId: "G-4XE38QMSBZ"
+};
